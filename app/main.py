b68d166b193d58de48bea3b74e0bc835edd36aac
Folami <folami.bidmus@gmail.com>
2025-05-02 15:45:47 +0100

    [Send Correlation ID]

--- app/main.py
+++ app/main.py
@@ -9,23 +9,20 @@ def main():
     client_socket, client_address = server.accept()
     print(f"Connection from {client_address} has been established!")
     
-    # For this stage:
-    # - message_size: 4 bytes (any value works; we'll use 0)
-    # - correlation_id: hard-coded to 7
-    # All integers are 32-bit signed integers in big-endian order.
+    # Build response:
+    # message_size: 4 bytes (any value works; we'll use 0)
+    # correlation_id: hard-coded to 7
+    # All integers are 32-bit signed in big-endian order.
     message_size = struct.pack('>i', 0)
     correlation_id = struct.pack('>i', 7)
     response = message_size + correlation_id
 
     client_socket.sendall(response)
+    # Gracefully shutdown the write side to ensure the client can read data.
+    client_socket.shutdown(socket.SHUT_WR)
     
     client_socket.close()
     server.close()
 
 if __name__ == "__main__":
     main()
-
-
-#In this implementation, when a client connects and sends a request, the server responds with an eight-byte message:
-#- Four bytes for the message size (here set to 0 for simplicity)
-#- Four bytes for the correlation ID (7)
